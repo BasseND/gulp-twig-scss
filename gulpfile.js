54dc416856461085ba001ad6b97c7919b0429fd3
Bassirou NDIAYE <ndiayebasse@gmail.com>
2020-03-01 15:35:12 +0100

    Settings Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,51 @@
 /*global require*/
 "use strict";
 
-var gulp = require('gulp'),
-    path = require('path'),
-    data = require('gulp-data'),
-	  twig = require('gulp-twig'), // Decided to use twig, because already familiar with it
-    prefix = require('gulp-autoprefixer'),
-    sass = require('gulp-sass'),    
-	  plumber = require('gulp-plumber'),
-    concat = require('gulp-concat'),
-    sourcemaps = require('gulp-sourcemaps'),
-	  browserSync = require('browser-sync'),
-	  fs = require('fs');
+var gulp = require("gulp"),
+  path = require("path"),
+  data = require("gulp-data"),
+  twig = require("gulp-twig"), // Decided to use twig, because already familiar with it
+  prefix = require("gulp-autoprefixer"),
+  sass = require("gulp-sass"),
+  plumber = require("gulp-plumber"),
+  concat = require("gulp-concat"),
+  minify = require('gulp-minify'),
+  sourcemaps = require("gulp-sourcemaps"),
+  browserSync = require("browser-sync"),
+  fs = require("fs");
 
 /*
  * Directories here
  */
 var paths = {
-  build: './build/',
-  sass: './src/scss/',
-  cssDS: './ds-sources/assets/css/',
-  css: './build/assets/css/',
-  data: './twig/data/'
+  build: "./build/",
+  // sass: "./src/scss/",
+  cssDS: "./ds-sources/assets/css/",
+  // css: "./build/assets/css/",
+  data: "./twig/data/",
+
+  sass: {
+    input: "./src/scss/",
+    output: "./build/assets/css/"
+  },
+
+  scripts: {
+    input: "./src/js/*",
+    output: "./build/assets/js/"
+    // polyfills: ".polyfill.js",
+  },
+  images: {
+    input: "./src/assets/images/",
+    output: "./build/assets/images/"
+  },
+  fonts: {
+    input: "./src/assets/fonts/",
+    output: "./build/assets/fonts/"
+  },
+  svgs: {
+    input: "./src/assets/svg/*.svg",
+    output: "./build/assets/svg/"
+  }
 };
 
 /**
@@ -72,46 +96,63 @@ gulp.task(
 
 gulp.task(
   "browser-sync",
-  gulp.series([gulpSassTask, gulpSassSourcesTask, gulpTwigTask, gulpJsTask], function() {
-    browserSync({
-      server: {
-        baseDir: paths.build
-      },
-      notify: false,
-      browser: ["google chrome", "firefox"]
-    });
-  })
+  gulp.series(
+    [
+      gulpSassTask,
+      gulpSassSourcesTask,
+      gulpTwigTask,
+      gulpJsTask,
+      gulpCopyImageTask,
+      gulpCopyFontsTask
+    ],
+    function() {
+      browserSync({
+        server: {
+          baseDir: paths.build
+        },
+        notify: false,
+        browser: ["google chrome", "firefox"]
+      });
+    }
+  )
 );
 
 /**
  * Compile .scss files into build css directory With autoprefixer no
  * need for vendor prefixes then live reload the browser.
  */
-function gulpSassSourcesTask() {
-  return gulp.src(paths.sass + 'ds/style.scss')
-    .pipe(sourcemaps.init())
-    // Stay live and reload on error
-    .pipe(plumber({
-      handleError: function (err) {
-        console.log(err);
-        this.emit('end');
-      }
-    }))
-    .pipe(sass({
-      includePaths: [paths.sass + 'ds/'],
-      outputStyle: 'expanded'
-    })
-      .on('error', function (err) {
-        console.log(err.message);
-        // sass.logError
-        this.emit('end');
-      })
-    )
-    .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], {
-      cascade: true
-    }))
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest(paths.cssDS));
+function gulpSassSourcesTask () {
+  return (
+    gulp
+      .src(paths.sass.input + "ds/style.scss")
+      .pipe(sourcemaps.init())
+      // Stay live and reload on error
+      .pipe(
+        plumber({
+          handleError: function(err) {
+            console.log(err);
+            this.emit("end");
+          }
+        })
+      )
+      .pipe(
+        sass({
+          includePaths: [paths.sass.input + "ds/"],
+          outputStyle: "expanded"
+        }).on("error", function(err) {
+          console.log(err.message);
+          // sass.logError
+          this.emit("end");
+        })
+      )
+      .pipe(
+        prefix(["last 15 versions", "> 1%", "ie 8", "ie 7"], {
+          cascade: true
+        })
+      )
+      .pipe(sourcemaps.write("."))
+      .pipe(gulp.dest(paths.cssDS))
+  );
 };
 
 
@@ -121,47 +162,68 @@ function gulpSassSourcesTask() {
  * need for vendor prefixes then live reload the browser.
  */
 function gulpSassTask  () {
-  return gulp.src(paths.sass + 'site/main.scss')
-    .pipe(sourcemaps.init())
-    // Stay live and reload on error
-    .pipe(plumber({
-      handleError: function (err) {
-        console.log(err);
-        this.emit('end');
-      }
-    }))
-    .pipe(sass({
-      includePaths: [paths.sass + 'site/', paths.sass + 'ds/'],
-		  outputStyle: 'expanded'
-		})
-		.on('error', function (err) {
-			console.log(err.message);
-			// sass.logError
-			this.emit('end');
-		})
-	)
-    .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], {
-      cascade: true
-    }))
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest(paths.css));
+  return (
+    gulp
+      .src(paths.sass.input + "site/main.scss")
+      .pipe(sourcemaps.init())
+      // Stay live and reload on error
+      .pipe(
+        plumber({
+          handleError: function(err) {
+            console.log(err);
+            this.emit("end");
+          }
+        })
+      )
+      .pipe(
+        sass({
+          includePaths: [paths.sass.input + "site/"],
+          outputStyle: "expanded"
+        }).on("error", function(err) {
+          console.log(err.message);
+          // sass.logError
+          this.emit("end");
+        })
+      )
+      .pipe(
+        prefix(["last 15 versions", "> 1%", "ie 8", "ie 7"], {
+          cascade: true
+        })
+      )
+      .pipe(sourcemaps.write("."))
+      .pipe(gulp.dest(paths.sass.output))
+  );
 };
 
 /**
  * Compile .js files into build js directory With app.min.js
  */
-function gulpJsTask (){
-    return gulp.src('build/assets/js/script.js')
-        .pipe(sourcemaps.init())
-        .pipe(concat('script.min.js'))
-        .on('error', function (err) {
-            console.log(err.toString());
-            this.emit('end');
-        })
-		.pipe(sourcemaps.write('.'))		
-		.pipe(gulp.dest('build/assets/js'));
+function gulpJsTask () {
+    return gulp
+      .src(paths.scripts.input)
+      .pipe(sourcemaps.init())
+      .pipe(concat("script.min.js"))
+      .pipe(minify())
+      .on("error", function(err) {
+        console.log(err.toString());
+        this.emit("end");
+      })
+      .pipe(sourcemaps.write("."))
+      .pipe(gulp.dest(paths.scripts.output));
 };
 
+ function gulpCopyImageTask () {    
+    return gulp
+      .src(paths.images.input + "/**/*.*")
+      .pipe(gulp.dest(paths.images.output));  
+};
+
+ function gulpCopyFontsTask () {    
+    return gulp
+      .src(paths.fonts.input + "/**/*.*")
+      .pipe(gulp.dest(paths.fonts.output));  
+};
+     
 /**
  * Watch scss files for changes & recompile
  * Watch .twig files run twig-rebuild then reload BrowserSync
@@ -169,13 +231,22 @@ function gulpJsTask (){
 function gulpWatchTask () {
 	  gulp.watch(paths.build + 'assets/js/script.js', ['js', browserSync.reload]);
     // gulp.watch(paths.sass + 'vendors/main.scss', ['sass', browserSync.reload]);
-    gulp.watch(paths.sass + 'ds/style.scss', ['sass', browserSync.reload]);
-    gulp.watch(paths.sass + 'site/main.scss', ['sass', browserSync.reload]);
+    gulp.watch(paths.sass.input + 'ds/style.scss', ['sass', browserSync.reload]);
+    gulp.watch(paths.sass.input + 'site/main.scss', ['sass', browserSync.reload]);
   	gulp.watch(['twig/templates/**/*.twig','twig/data/*.twig.json'], {cwd:'./'}, ['rebuild']);
 };
 
 // Build task compile sass and twig.
-gulp.task("build", gulp.series([gulpSassTask, gulpSassSourcesTask, gulpTwigTask]));
+gulp.task(
+  "build",
+  gulp.series([
+    gulpSassTask,
+    gulpSassSourcesTask,
+    gulpTwigTask,
+    gulpCopyImageTask,
+    gulpCopyFontsTask
+  ])
+);
 
 /**
  * Default task, running just `gulp` will compile the sass,
