f0b07b6efe89603020aef1a20c973f80e57280f8
Bassirou NDIAYE <ndiayebasse@gmail.com>
2020-03-04 14:54:57 +0100

    Sidebar menu

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   // Sidebar Menu
-  $("#leftside-navigation .sub-menu > a").click(function(e) {
-    $("#leftside-navigation ul ul").slideUp(),
+  $("#side-menu .sub-menu > a").click(function(e) {
+    $("#side-menu ul ul").slideUp(),
       $(this)
         .next()
         .is(":visible") ||
@@ -10,4 +10,23 @@ $(document).ready(function() {
           .slideDown(),
       e.stopPropagation();
   });
+
+   // Metis Menu 
+
+  $("#side-menu").metisMenu();
+
+      $("#sidebar-menu a").each(function() {
+        var t = window.location.href.split(/[?#]/)[0];
+        this.href == t && ($(this).addClass("active"),
+        $(this).parent().addClass("mm-active"),
+        $(this).parent().parent().addClass("mm-show"),
+        $(this).parent().parent().prev().addClass("mm-active"),
+        $(this).parent().parent().parent().addClass("mm-active"),
+        $(this).parent().parent().parent().parent().addClass("mm-show"),
+        $(this).parent().parent().parent().parent().parent().addClass("mm-active"))
+    });
+
+ 
 });
+
+
